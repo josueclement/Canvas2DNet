2470f01fce70003db67c3228864f8a266c61ffe5
Josué Clément <mod6991@gmail.com>
2023-02-16 10:53:02 +0100

    Add path drawing object

---
+++ Canvas2DNet/DrawingObjects/Canvas2DPath.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows.Media;
+
+namespace Canvas2DNet.DrawingObjects
+{
+    /// <summary>
+    /// Path drawing object
+    /// </summary>
+    public class Canvas2DPath : Canvas2DShape
+    {
+        #region Properties
+
+        /// <summary>
+        /// Path geometry data
+        /// </summary>
+        public Geometry? Data
+        {
+            get => _data;
+            set => SetProperty(ref _data, value);
+        }
+        private Geometry? _data;
+
+        #endregion
+    }
+}
--- Canvas2DNetTester/ViewModel/MainWindowViewModel.cs
+++ Canvas2DNetTester/ViewModel/MainWindowViewModel.cs
@@ -2,6 +2,7 @@
 using Canvas2DNet.DrawingObjects;
 using CommunityToolkit.Mvvm.ComponentModel;
 using System.Collections.ObjectModel;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 
@@ -38,6 +39,17 @@ namespace Canvas2DNetTester.ViewModel
                 Height = 20,
                 Stroke = new SolidColorBrush(Colors.Black)
             });
+            MyItems.Add(new Canvas2DPath
+            {
+                X = 190,
+                Y = 20,
+                Width = 50,
+                Height = 50,
+                StrokeThickness = new Thickness(1),
+                Stroke = new SolidColorBrush(Colors.Black),
+                Fill = new SolidColorBrush(Colors.Red),
+                Data = GetGeometry()
+            });
             MyItems.Add(new TestObject
             {
                 X = 300,
@@ -46,6 +58,23 @@ namespace Canvas2DNetTester.ViewModel
                 Height = 25,
                 Content = "Blah"
             });
+        }
+
+        private Geometry GetGeometry()
+        {
+            StreamGeometry stream = new StreamGeometry();
+            stream.FillRule = FillRule.EvenOdd;
+
+            using (StreamGeometryContext ctx = stream.Open())
+            {
+                ctx.BeginFigure(new Point(0, 0), true, false);
+                ctx.LineTo(new Point(40, 0), true, false);
+                ctx.LineTo(new Point(20, 40), true, false);
+                ctx.LineTo(new Point(0, 0), true, false);
+
+                stream.Freeze();
+                return stream;
+            }
         }
 
         public ObservableCollection<DrawingObject> MyItems { get; set; } = new ObservableCollection<DrawingObject>();
