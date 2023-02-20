4378c565f3f3e5bcc84d25a3497d0b24556f37f7
Josué Clément <mod6991@gmail.com>
2023-02-20 16:59:04 +0100

    Remove part and CanvasLoaded event

--- Canvas2DNet/Canvas2D.cs
+++ Canvas2DNet/Canvas2D.cs
@@ -10,22 +10,9 @@ namespace Canvas2DNet
     /// </summary>
     public class Canvas2D : Control
     {
-        private FrameworkElement? PART_container;
-
         static Canvas2D()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(Canvas2D), new FrameworkPropertyMetadata(typeof(Canvas2D)));
-        }
-
-        /// <inheritdoc/>
-        public override void OnApplyTemplate()
-        {
-            PART_container = (FrameworkElement)Template.FindName("PART_container", this);
-            if (PART_container != null )
-            {
-                PART_container.Loaded += (s, e) => RaiseCanvasLoadedEvent();
-            }
-            base.OnApplyTemplate();
         }
 
         #region Dependency properties
@@ -73,37 +60,6 @@ namespace Canvas2DNet
         {
             get { return (Canvas2DInteractions)GetValue(CanvasInteractionsProperty); }
             set {  SetValue(CanvasInteractionsProperty, value); }
-        }
-
-        #endregion
-
-        #region Events
-
-        /// <summary>
-        /// Routed event for CanvasLoaded
-        /// </summary>
-        public static readonly RoutedEvent CanvasLoadedEvent = EventManager.RegisterRoutedEvent(
-            name: nameof(CanvasLoaded),
-            routingStrategy: RoutingStrategy.Bubble,
-            handlerType: typeof(RoutedEventHandler),
-            ownerType: typeof(Canvas2D));
-
-        /// <summary>
-        /// Occurs when the canvas is loaded
-        /// </summary>
-        public event RoutedEventHandler CanvasLoaded
-        {
-            add { AddHandler(CanvasLoadedEvent, value); }
-            remove { RemoveHandler(CanvasLoadedEvent, value); }
-        }
-
-        /// <summary>
-        /// Raise the CanvasLoadedEvent RoutedEvent
-        /// </summary>
-        protected void RaiseCanvasLoadedEvent()
-        {
-            RoutedEventArgs routedEventArgs = new(routedEvent: CanvasLoadedEvent);
-            RaiseEvent(routedEventArgs);
         }
 
         #endregion
